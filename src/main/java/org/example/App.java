ce53a304c971ab2acc90c3221b25949709e683d6
maximagarin <mmaximagarin@gmail.com>
2022-04-21 10:58:59 +0300

    евритхинг

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,15 +13,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class App 
 {
     public static void main( String[] args ) throws InterruptedException {
-       System.setProperty("webdriver.chrome.driver", "/Users/maximagarin/Desktop/Driver/chromedriver");
-        WebDriver driver = new ChromeDriver();
-       //WebDriver driver = WebDriverManager.chromedriver().create();
+       //System.setProperty("webdriver.chrome.driver", "/Users/maximagarin/Desktop/Driver/chromedriver");
+        //WebDriver driver = new ChromeDriver();
+       WebDriver driver = WebDriverManager.chromedriver().create();
         driver.get("https://www.proskater.ru/");
         driver.manage().window().maximize();
 
 
         WebElement enter = driver.findElement(By.xpath("//a[@class='login-btn']"));
-        driver.quit();
+        enter.click();
 
 
 
